a1171dbf0671fe6b85ff08217b50bb79e0c17e32
HillTopTRPG <quoridorn@gmail.com>
2023-06-11 19:57:55 +0900

    :sparkles: [SAM][AppSync] ログインまで実装

--- sam/manchat-app-sync/app-sync/lambda/nodejs/authorizer.ts
+++ sam/manchat-app-sync/app-sync/lambda/nodejs/authorizer.ts
@@ -1,38 +1,48 @@
-import {DynamoDBClient} from '@aws-sdk/client-dynamodb'
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
-import { uuid } from 'uuidv4'
 export const ddbDocClient = DynamoDBDocumentClient.from(new DynamoDBClient({}))
 
-import { GetCommand, PutCommand, GetCommandOutput } from '@aws-sdk/lib-dynamodb'
+import { GetCommand, GetCommandOutput } from '@aws-sdk/lib-dynamodb'
 
-const ALL_OPERATIONS = [
-  'Query.getConnectionId',
-  'Query.getRooms',
-  'Query.getUsers',
-  'Query.getChats',
-  'Mutation.addRoom',
-  'Mutation.entryRoom',
-  'Mutation.signUp',
-  'Mutation.signIn',
-  'Mutation.addChat',
-]
+type ConnectionData = {
+  id: string,
+  roomId?: string,
+  userId?: string
+}
 
-const connectionTableName = process.env.CONNECTION_TABLE_NAME
-const roomTableName = process.env.ROOM_TABLE_NAME
-const userTableName = process.env.USER_TABLE_NAME
+type RoomData = {
+  id: string,
+  token: string
+}
 
-export const handler = async (event) => {
-  console.log(`event >`, JSON.stringify(event, null, 2))
+type UserData = {
+  id: string,
+  token: string
+}
 
-  const {
-    authorizationToken,
-    requestContext: { apiId, accountId },
-  } = event
+async function getData<T>(tableName: string, id: string): Promise<T | undefined> {
+  let result: T | undefined = undefined
+  try {
+    const data: GetCommandOutput = await ddbDocClient.send<T>(new GetCommand({
+      TableName : tableName,
+      Key: { id }
+    }))
+    result = data?.Item as T || undefined
+  } catch (err) {
+    // do nothing
+  }
+  return result
+}
 
-  const typesBase = `arn:aws:appsync:${process.env.AWS_REGION}:${accountId}:apis/${apiId}/types`
-  const definedTypeFieldsBase = []
-  const admitFields: string[] = []
+async function checkToken<T extends { token: string }>(tableName: string, id: string, token: string | null): Promise<boolean> {
+  if (!token) {
+    return false
+  }
+  let result = await getData<T>(tableName, id)
+  return result?.token === token
+}
 
+async function fetchConnectionInfo(authorizationToken: string) {
   const authSplit = authorizationToken.split('/')
 
   let connectionId = authSplit[0]
@@ -45,112 +55,103 @@ export const handler = async (event) => {
     userToken = authSplit[2]
   }
 
-  type ConnectionData = {
-    id: string,
-    roomId?: string,
-    userId?: string
-  }
-  let connectionData: ConnectionData | null = null
-
-  type RoomData = {
-    id: string,
-    token: string
-  }
-  let roomData: RoomData | null = null
-
-  type UserData = {
-    id: string,
-    token: string
-  }
-  let userData: UserData | null = null
-
-  try {
-    const data: GetCommandOutput = await ddbDocClient.send<ConnectionData>(new GetCommand({
-      TableName : connectionTableName,
-      Key: { id: connectionId },
-    }))
-    connectionData = data?.Item as ConnectionData || null
-  } catch (err) {
-    // do nothing
-  }
+  const connectionData = await getData<ConnectionData>(process.env.CONNECTION_TABLE_NAME, connectionId)
 
   if (!connectionData) {
-    // 初回接続の際はconnectionIdの取得が最優先
-    admitFields.push('Query.getConnectionId')
-    connectionId = uuid()
-    await ddbDocClient.send(new PutCommand({
-      TableName : connectionTableName,
-      Item: {
-        id: connectionId,
-      },
-    }))
-  } else if (!connectionData.roomId) {
-    // 未入室状態なら入室が最優先
+    connectionId = null
+  }
+  let roomId = connectionData?.roomId || null
+  let userId = connectionData?.userId || null
+
+  if (roomId === 'Empty') {
+    roomId = null
+  }
+  if (userId === 'Empty') {
+    userId = null
+  }
+
+  const result = {
+    connectionId,
+    roomId,
+    userId,
+    isAuthorized: true
+  }
+
+  if (!connectionId || !roomId) {
+    return result
+  }
+  result.isAuthorized = await checkToken<RoomData>(process.env.ROOM_TABLE_NAME, roomId, roomToken)
+  if (!result.isAuthorized || !userId) {
+    return result
+  }
+  result.isAuthorized = await checkToken<UserData>(process.env.USER_TABLE_NAME, userId, userToken)
+  return result
+}
+
+export const handler = async (event) => {
+  console.log(`event >`, JSON.stringify(event, null, 2))
+
+  const {
+    connectionId,
+    roomId,
+    userId
+  } = await fetchConnectionInfo(event.authorizationToken)
+
+  // const typesBase = `arn:aws:appsync:${process.env.AWS_REGION}:${event.accountId}:apis/${event.apiId}/types`
+  const definedTypeFieldsBase = [
+    // `${typesBase}/Room/fields/password`,
+  ]
+  const admitFields: string[] = []
+
+  const ALL_OPERATIONS = [
+    'Mutation.init',
+    'Query.getRooms',
+    'Query.getUsers',
+    'Query.getChats',
+    'Mutation.addRoom',
+    'Mutation.entryRoom',
+    'Mutation.signUp',
+    'Mutation.signIn',
+    'Mutation.addChat',
+  ]
+
+  if (!connectionId) {
+    // 初回接続
+    admitFields.push('Mutation.init')
+  } else if (!roomId) {
+    // 未入室状態
     admitFields.push('Query.getRooms')
     admitFields.push('Mutation.addRoom')
     admitFields.push('Mutation.entryRoom')
+  } else if (!userId) {
+    // 未ログイン状態
+    admitFields.push('Query.getUsers')
+    admitFields.push('Mutation.signUp')
+    admitFields.push('Mutation.signIn')
+    // 部屋検索はしてもいいよ
+    admitFields.push('Query.getRooms')
   } else {
-    // 入室状態
-    try {
-      const data: GetCommandOutput = await ddbDocClient.send<RoomData>(new GetCommand({
-        TableName : roomTableName,
-        Key: { id: connectionData.roomId },
-      }))
-      roomData = data?.Item as RoomData || null
-    } catch (err) {
-      // do nothing
-    }
-    if (!roomData || roomData.token !== roomToken) {
-      // 認証失敗
-      return {
-        isAuthorized: false,
-        ttlOverride: 0
-      }
-    }
-
-    if (!connectionData.userId) {
-      // 未ログイン状態ならログインが最優先
-      admitFields.push('Query.getUsers')
-      admitFields.push('Mutation.signUp')
-      admitFields.push('Mutation.signIn')
-    } else {
-      // ログイン状態
-      try {
-        const data: GetCommandOutput = await ddbDocClient.send<UserData>(new GetCommand({
-          TableName : userTableName,
-          Key: { id: connectionData.userId },
-        }))
-        userData = data?.Item as UserData || null
-      } catch (err) {
-        // do nothing
-      }
-      if (!userData || userData.token !== userToken) {
-        // 認証失敗
-        return {
-          isAuthorized: false,
-          ttlOverride: 0
-        }
-      }
-      admitFields.push('Query.getChats')
-      admitFields.push('Mutation.addChat')
-    }
+    // ログイン済
+    admitFields.push('Query.getChats')
+    admitFields.push('Mutation.addChat')
+    // 部屋検索はしてもいいよ
+    admitFields.push('Query.getRooms')
   }
 
   const response = {
-    // ここがfalseの場合は認証失敗になる
     isAuthorized: true,
-    // 後続処理に渡せる情報。vtlやリゾルバーに渡す値として利用できる
-    // ここで権限チェック系の共通処理の結果を渡してあげるとよさそう
     resolverContext: {
-      connectionId: connectionId,
+      connectionId,
+      roomId,
+      userId
     },
-    // 拒否するfieldを選択できる
     deniedFields: [
       ...definedTypeFieldsBase,
       ...ALL_OPERATIONS.filter(d => admitFields.every(a => a !== d)),
     ],
     ttlOverride: 0,
   }
+
   console.log(`response >`, JSON.stringify(response, null, 2))
   return response;
 }
